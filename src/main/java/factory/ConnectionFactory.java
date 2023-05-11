a5d52085629ec4ee4b626df19cb82735dc778f90
Gerardo Sanchez <114122029+xeras55@users.noreply.github.com>
2023-05-11 09:11:42 -0600

    first commit

---
+++ src/main/java/factory/ConnectionFactory.java
@@ -0,0 +1,30 @@
+package factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+  public DataSource dataSource;
+
+  public ConnectionFactory(){
+    ComboPooledDataSource pooledDataSource = new ComboPooledDataSource();
+    pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimezone=UTC");
+    pooledDataSource.setUser("root");
+    pooledDataSource.setPassword("Yanosequemas55#");
+    pooledDataSource.setMaxPoolSize(10);
+
+    this.dataSource = pooledDataSource;
+  }
+  
+  public Connection recuperaConexion(){
+    try{
+    return this.dataSource.getConnection();
+  }catch(SQLException e){
+    throw new RuntimeException(e);
+  }
+  }
+}
